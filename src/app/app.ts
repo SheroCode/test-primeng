8a8e6b7373e8416a59f1c0257384d45315adc5d7
Shereen Mohamed <shereen.khalafallah@gmail.com>
2025-06-24 14:25:10 +0300

    add navbar card button to test

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Menubar, MenubarModule } from 'primeng/menubar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ButtonModule,CardModule,Menubar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'primeNG-test';
+export class App implements OnInit {
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home'
+            },
+            {
+                label: 'Features',
+                icon: 'pi pi-star'
+            },
+            {
+                label: 'Projects',
+                icon: 'pi pi-search',
+                items: [
+                    {
+                        label: 'Components',
+                        icon: 'pi pi-bolt'
+                    },
+                    {
+                        label: 'Blocks',
+                        icon: 'pi pi-server'
+                    },
+                    {
+                        label: 'UI Kit',
+                        icon: 'pi pi-pencil'
+                    },
+                    {
+                        label: 'Templates',
+                        icon: 'pi pi-palette',
+                        items: [
+                            {
+                                label: 'Apollo',
+                                icon: 'pi pi-palette'
+                            },
+                            {
+                                label: 'Ultima',
+                                icon: 'pi pi-palette'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Contact',
+                icon: 'pi pi-envelope'
+            }
+        ]
+    }
 }
